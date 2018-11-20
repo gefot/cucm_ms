02920e9679489af76bf86f8d2ec70e6529d958a3
gefot <georgios.fotiadis@gmail.com>
2018-11-20 13:39:16 +0200

    New

--- modules/module_cucm_funcs.py
+++ modules/module_cucm_funcs.py
@@ -6,6 +6,7 @@ from suds.xsd.doctor import ImportDoctor    # suds-jurko
 from suds.client import Client      # suds-jurko
 # from suds import WebFault         # suds-jurko
 import os
+import ssl
 from pathlib import Path
 
 
@@ -14,18 +15,23 @@ def cucm_axl_query(CM_CREDS, command, query):
     '''
     Connect to CUCM AXL interface and execute a SOAP query based on one of the three functions
     (executeSQLQuery, getPhone, listPhone) using a predefined WSDL file, which is stored locally.
+
+    Used in: get_configured devices
     '''
     try:
+        my_path = '/' + str(Path(os.getcwd()).parent).replace('\\', '/') + '/data/'     # Windows
+        wsdl_file_location = 'file://{}AXLAPI.wsdl'.format(my_path)
+
         tns = 'http://schemas.cisco.com/ast/soap/'
         imp = Import('http://schemas.xmlsoap.org/soap/encoding/', 'http://schemas.xmlsoap.org/soap/encoding/')
         imp.filter.add(tns)
 
-        # wsdl_file_location = 'file://{}AXLAPI.wsdl'.format('/stats/mrtg/scripts/voip_stats/'      # Linux
-        # wsdl_file_location = 'file://{}AXLAPI.wsdl'.format('/D:/_gfot/PyCharmProjects/cucm/')     # Windows
-        my_path = '/' + str(Path(os.getcwd()).parent).replace('\\', '/') + '/data/'
-        wsdl_file_location = 'file://{}AXLAPI.wsdl'.format(my_path)  # Windows
+        url_location = 'https://{}:{}/axl/'.format(CM_CREDS['cm_server_ip_address'], CM_CREDS['cm_server_port'])
 
-        url_location = 'https://{}:{}/axl/'.format(CM_CREDS['cm_server_hostname'], CM_CREDS['cm_server_port'])
+        # Bypass certificate warning
+        if hasattr(ssl, '_create_unverified_context'):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
         client = Client(wsdl_file_location, location=url_location, username=CM_CREDS['soap_user'], \
                         password=CM_CREDS['soap_pass'], plugins=[ImportDoctor(imp)], faults=False)
 
@@ -42,10 +48,38 @@ def cucm_axl_query(CM_CREDS, command, query):
 
 
 #################################################################################
+def cucm_risport_query(CM_CREDS, command, query):
+    '''
+    Connect to CUCM RisPort interface using SOAP
+    '''
+    try:
+        tns = 'http://schemas.cisco.com/ast/soap/'
+        imp = Import('http://schemas.xmlsoap.org/soap/encoding/', 'http://schemas.xmlsoap.org/soap/encoding/')
+        imp.filter.add(tns)
+
+        wsdl = 'https://{}:{}/realtimeservice/services/RisPort?wsdl'.format(CM_CREDS['cm_server_ip_address'],
+                                                                            CM_CREDS['cm_server_port'])
+        location = 'https://{}:{}/realtimeservice/services/RisPort'.format(CM_CREDS['cm_server_ip_address'],
+                                                                           CM_CREDS['cm_server_port'])
+        # Bypass certificate warning
+        if hasattr(ssl, '_create_unverified_context'):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+        client = Client(wsdl, location=location, username=CM_CREDS['soap_user'], password=CM_CREDS['soap_pass'],
+                        plugins=[ImportDoctor(imp)])
+        if command == 'SelectCmDevice':
+            return client.service.SelectCmDevice('', query)
+
+    except Exception as ex:
+        print("cucm_risport_query exception: ", ex)
+        return None
+
+
+#################################################################################
 def cucm_get_configured_devices(CM_CREDS):
     '''
     Takes CUCM credentials dictionary as argument
-    Returns all CUCM configured devices as a list [mac_address, description, extension, device type]
+    Returns all CUCM configured devices as a list [mac_address, extension, description, device type]
     '''
     try:
         sql_query = "select a.name, a.description, b.dnorpattern, b.alertingname, c.display, d.name as type from device as a, \
@@ -57,8 +91,8 @@ def cucm_get_configured_devices(CM_CREDS):
         device_list_full = result[1]['return'].row
         all_configured_devices = []
         for dev in device_list_full:
-            if dev.name.startswith(("SEP", "ATA", "AALN")):
-                temp_dev = [str(dev.name), str(dev.description), str(dev.dnorpattern), str(dev.type)]
+            if dev.name.startswith(("SEP", "ATA", "AALN", "AN", "CSF")):
+                temp_dev = [str(dev.name), str(dev.description), str(dev.dnorpattern), str(dev.alertingname), str(dev.type)]
                 all_configured_devices.append(temp_dev)
             else:
                 continue
@@ -73,21 +107,26 @@ def cucm_get_configured_devices(CM_CREDS):
 #################################################################################
 def cucm_count_interering_devices(devices):
     '''
-    Count CUCM devices (Total, IP Phones, ATA ports, ATA devices, MGCP Analog).
+    Count CUCM devices: Total(0), IP Phones(1), ATA devices(2), ATA ports(3), Analog(4), Jabber(5)
     Returns a list of numbers with the number of appearances.
     '''
     try:
-        device_count = [0] * 5
+        device_count = [0] * 6
         for device in devices:
-            device_count[0] += 1
             if device[0].startswith("SEP"):
+                device_count[0] += 1
                 device_count[1] += 1
             elif device[0].startswith("ATA"):
-                device_count[2] += 1
-            if device[0].startswith("ATA") and not device[0].endswith("01"):
-                device_count[3] += 1
-            elif device[0].startswith("AA") and not device[0].startswith("ANN"):
+                device_count[3] += 1        # ATA port count
+                if not device[0].endswith("01"):
+                    device_count[0] += 1
+                    device_count[2] += 1    # ATA device count
+            elif device[0].startswith("AN") or device[0].startswith("AALN"):
+                device_count[0] += 1
                 device_count[4] += 1
+            elif device[0].startswith("CSF"):
+                device_count[0] += 1
+                device_count[5] += 1
 
         return device_count
 
@@ -96,24 +135,37 @@ def cucm_count_interering_devices(devices):
         return [0] * 5
 
 
-
 #################################################################################
-# # Connect to CUCM RisPort interface using SOAP
-# def cucm_risport(cmserver, cmport, soap_user, soap_pass, command, query):
-#     try:
-#         tns = 'http://schemas.cisco.com/ast/soap/'
-#         imp = Import('http://schemas.xmlsoap.org/soap/encoding/', 'http://schemas.xmlsoap.org/soap/encoding/')
-#         imp.filter.add(tns)
-#         wsdl = 'https://' + cmserver + ':' + cmport + '/realtimeservice/services/RisPort?wsdl'
-#         location = 'https://' + cmserver + ':' + cmport + '/realtimeservice/services/RisPort'
-#
-#         client = Client(wsdl, location=location, username=soap_user, password=soap_pass, plugins=[ImportDoctor(imp)])
-#         if command == 'SelectCmDevice':
-#             return client.service.SelectCmDevice('', query)
-#
-#     except:
-#         return None
+def cucm_fill_devices_status(CM_CREDS, all_devices):
+    '''
+    Use RIS interface to get registration status and timestamps for all devices
+    '''
+    try:
+        command = "SelectCmDevice"
+        query = {'SelectBy': 'Name', 'Status': 'Any', 'Class': 'Any', 'MaxReturnedDevices': '5000'}
 
+        result = cucm_risport_query(CM_CREDS, command, query)
+
+        for device in all_devices:
+            found_device = False
+            for node in result['SelectCmDeviceResult']['CmNodes']:
+                if node['Name'] in CM_CREDS['cm_server_ip_address']:
+                    for status_device in node['CmDevices']:
+                        if device[0] == status_device['Name']:
+                            found_device = True
+                            timestamp = datetime.datetime.fromtimestamp(int(status_device['TimeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
+                            device.append(str(status_device['Status']).lower())
+                            device.append(timestamp)
+
+            if not found_device:
+                device.append("unregistered")
+                device.append("More than a week")
+
+        return all_devices
+
+    except Exception as ex:
+        print("cucm_fill_devices_status exception: ", ex)
+        return None
 
 #################################################################################
 # Connect to CUCM PerfmonPort interface using SOAP
